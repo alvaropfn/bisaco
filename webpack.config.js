9118468767b493dadfae43f53a8812b039c76e67
alvaropfn <alvaropfn@gmail.com>
2016-07-01 09:18:11 -0300

    instalação das configurações minimas para utilização do react

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,29 @@
 var config = {
-	devServer:{
-		inline: true,
-		port:8888
-	}
+   entry: './main.js',
+
+   output: {
+      path:'./',
+      filename: 'index.js',
+   },
+
+   devServer: {
+      inline: true,
+      port: 8888
+   },
+
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
 }
+
+module.exports = config;
